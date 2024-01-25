a8a0b7a9e066ff68f7811bf5aa72c92e579d3919
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2024-01-25 12:52:37 +0100

    Tentativo snack

---
+++ oop/snakc_opp.php
@@ -0,0 +1,16 @@
+<?php
+
+class Pokemon {
+
+}
+
+$pikachu = new Pokemon();
+$pikachu->nome = "Pikachu";
+$pikachu->colore = "Giallo";
+$pikachu->tipo = "Elettro";
+$pikachu->specie = "Pokemon topo";
+$pikachu->autore = "Atsuko Nishida";
+
+var_dump($pikachu)
+
+?>
